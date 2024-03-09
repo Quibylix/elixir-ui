65f67ce53f673690838b7fc1862ce76de97669f5
Quibylix <barraza.fredimanuel@gmail.com>
2024-03-08 21:26:56 -0600

    feat(apps/docs): add Navbar component to page

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,5 +1,6 @@
 import elixirUILogo from "@/assets/elixir-ui-logo.webp";
 import { getTranslation } from "@/features/i18n/services/get-translation.service";
+import Navbar from "@/features/ui/components/navbar/navbar.component";
 import { Button, Link } from "@elixir-ui/react";
 import Image from "next/image";
 import NextLink from "next/link";
@@ -9,52 +10,55 @@ export default async function HomePage() {
   const dict = await getTranslation();
 
   return (
-    <main className={styles.main}>
-      <section className={styles.hero}>
-        <Image
-          className={styles.logo}
-          src={elixirUILogo}
-          alt={dict.home.logo.alt}
-        />
-        <h1 className={styles.heading}>
-          <span className={styles.primaryColored}>
-            {dict.home.hero.heading.yourToolkit}
-          </span>{" "}
-          {dict.home.hero.heading.forCraftingExceptional}{" "}
-          <span className={styles.warningColored}>
-            {dict.home.hero.heading.userInterfaces}
-          </span>
-        </h1>
-        <p className={styles.description}>
-          <Link variant="text" href="#" color="primary">
-            {dict.home.hero.description.elixirUI}
-          </Link>{" "}
-          {dict.home.hero.description.isA}
-        </p>
-        <div className={styles.buttons}>
-          <Button
-            className={styles.button}
-            as={NextLink}
-            href="/docs"
-            size="lg"
-            color="primary"
-          >
-            {dict.home.hero.cta.getStarted}
-          </Button>
-          <Button
-            className={styles.button}
-            as={NextLink}
-            href="https://github.com/quibylix/elixir-ui"
-            target="_blank"
-            rel="noopener noreferrer"
-            size="lg"
-            variant="outlined"
-            color="secondary"
-          >
-            {dict.home.hero.cta.viewOnGitHub}
-          </Button>
-        </div>
-      </section>
-    </main>
+    <>
+      <Navbar />
+      <main className={styles.main}>
+        <section className={styles.hero}>
+          <Image
+            className={styles.logo}
+            src={elixirUILogo}
+            alt={dict.home.logo.alt}
+          />
+          <h1 className={styles.heading}>
+            <span className={styles.primaryColored}>
+              {dict.home.hero.heading.yourToolkit}
+            </span>{" "}
+            {dict.home.hero.heading.forCraftingExceptional}{" "}
+            <span className={styles.warningColored}>
+              {dict.home.hero.heading.userInterfaces}
+            </span>
+          </h1>
+          <p className={styles.description}>
+            <Link variant="text" href="#" color="primary">
+              {dict.home.hero.description.elixirUI}
+            </Link>{" "}
+            {dict.home.hero.description.isA}
+          </p>
+          <div className={styles.buttons}>
+            <Button
+              className={styles.button}
+              as={NextLink}
+              href="/docs"
+              size="lg"
+              color="primary"
+            >
+              {dict.home.hero.cta.getStarted}
+            </Button>
+            <Button
+              className={styles.button}
+              as={NextLink}
+              href="https://github.com/quibylix/elixir-ui"
+              target="_blank"
+              rel="noopener noreferrer"
+              size="lg"
+              variant="outlined"
+              color="secondary"
+            >
+              {dict.home.hero.cta.viewOnGitHub}
+            </Button>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
