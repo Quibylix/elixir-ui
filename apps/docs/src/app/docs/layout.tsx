92a50563811d2687b38cbec473c569c0a0530163
Quibylix <barraza.fredimanuel@gmail.com>
2024-03-12 12:08:18 -0600

    feat(apps/docs): add layout for /docs pages

---
+++ apps/docs/src/app/docs/layout.tsx
@@ -0,0 +1,21 @@
+import Navbar from "@/features/ui/components/navbar/navbar.component";
+import SideNavigationBar from "@/features/ui/components/side-navigation-bar/side-navigation-bar.component";
+import styles from "./layout.module.css";
+
+export default function DocsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <Navbar />
+        <div className={styles.container}>
+          <SideNavigationBar />
+          <main className={styles.main}>{children}</main>
+        </div>
+      </div>
+    </>
+  );
+}
