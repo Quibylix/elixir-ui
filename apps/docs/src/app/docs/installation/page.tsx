cb139de134c85c081c100a1d0673838fb598b2ac
Quibylix <barraza.fredimanuel@gmail.com>
2024-03-12 19:47:30 -0600

    feat(apps/docs): add installation page

---
+++ apps/docs/src/app/docs/installation/page.tsx
@@ -0,0 +1,153 @@
+import { getTranslation } from "@/features/i18n/services/get-translation.service";
+import { Link } from "@elixir-ui/react";
+import { Metadata } from "next";
+import NextLink from "next/link";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const dict = (await getTranslation()).docs.installation;
+
+  return dict.metadata;
+}
+
+export default async function InstallationPage() {
+  const dict = (await getTranslation()).docs.installation;
+
+  return (
+    <>
+      <h1>{dict.heading}</h1>
+      <p>
+        {dict.description.elixirUIIsDistributedAsAnNpmPackage}{" "}
+        <Link as={NextLink} color="warning" external href="https://pnpm.io/">
+          pnpm
+        </Link>{" "}
+        {dict.description.forAFasterAndMoreEfficientInstallation}
+      </p>
+      <h2>{dict.requirements.heading}</h2>
+      <ul>
+        <li>
+          <Link
+            as={NextLink}
+            color="success"
+            external
+            href="https://nodejs.org/en/"
+          >
+            Node.js
+          </Link>
+          . {dict.requirements.list.node}
+        </li>
+        <li>
+          <Link
+            as={NextLink}
+            color="secondary"
+            external
+            href="https://reactjs.org/"
+          >
+            React
+          </Link>
+          . {dict.requirements.list.react}
+        </li>
+        <li>
+          {dict.requirements.list.cssModules}
+          <Link
+            as={NextLink}
+            color="info"
+            external
+            href="https://github.com/css-modules/css-modules"
+          >
+            CSS Modules
+          </Link>
+          .
+        </li>
+        <li>
+          {dict.requirements.list.packageManager}
+          <Link
+            as={NextLink}
+            color="danger"
+            external
+            href="https://www.npmjs.com/"
+          >
+            npm
+          </Link>
+          ,{" "}
+          <Link as={NextLink} color="info" external href="https://yarnpkg.com/">
+            yarn
+          </Link>
+          {dict.requirements.list.or}{" "}
+          <Link as={NextLink} color="warning" external href="https://pnpm.io/">
+            pnpm
+          </Link>
+          .
+        </li>
+      </ul>
+      <h2>{dict.installationSteps.heading}</h2>
+      <p>{dict.installationSteps.steps.installPackage}</p>
+      <pre>
+        <code>
+          {`pnpm install @elixir-ui/react
+# or
+npm install @elixir-ui/react
+# or
+yarn add @elixir-ui/react`}
+        </code>
+      </pre>
+      <p>
+        {dict.installationSteps.steps.addLayers}
+        <Link as={NextLink} href="/docs/usage" color="primary">
+          {dict.installationSteps.steps.usage}
+        </Link>{" "}
+        {dict.installationSteps.steps.section}
+      </p>
+      <p>
+        {dict.installationSteps.steps.usesCSSLayers}
+        <Link
+          as={NextLink}
+          color="primary"
+          external
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS/@layer"
+        >
+          {dict.installationSteps.steps.mdnWebDocs}
+        </Link>
+        .
+      </p>
+      <p>{dict.installationSteps.steps.mustAddLayer}</p>
+      <pre>
+        <code>{`@layer variables, elixir;`}</code>
+      </pre>
+      <p>{dict.installationSteps.steps.inAddition}</p>
+      <pre>
+        <code>
+          {`@layer globals, variables, elixir;
+
+@layer globals {
+  /* Your global styles goes here, for example: */
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+}`}
+        </code>
+      </pre>
+      <p>{dict.installationSteps.steps.finally}</p>
+      <pre>
+        <code>
+          {`import { Button } from "@elixir-ui/react";
+
+export default function App() {
+  return (
+    <Button color="primary">Hello world!</Button>
+  );
+}`}
+        </code>
+      </pre>
+      <h2>{dict.nextSteps.heading}</h2>
+      <p>
+        {dict.nextSteps.description.nowThatYouHaveInstalled}
+        <Link as={NextLink} href="/docs/components/button" color="primary">
+          {dict.nextSteps.description.components}
+        </Link>{" "}
+        {dict.nextSteps.description.section}
+      </p>
+    </>
+  );
+}
