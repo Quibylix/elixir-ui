1ff38e3de5fe9ff0b179c0c7e6fe9d52e2de7fc8
Quibylix <barraza.fredimanuel@gmail.com>
2024-03-14 18:51:55 -0600

    feat(apps/docs): add language flags constant

--- apps/docs/src/features/i18n/constants/languages.constant.ts
+++ apps/docs/src/features/i18n/constants/languages.constant.ts
@@ -2,3 +2,8 @@ export const LANGUAGES = ["en", "es"] as const;
 export const DEFAULT_LANGUAGE = "en";
 export const LANGUAGE_COOKIE_NAME = "lang";
 export const ACCEPT_LANGUAGE_HEADER_NAME = "accept-language";
+
+export const LANGUAGE_FLAGS: Record<(typeof LANGUAGES)[number], string> = {
+  en: "🇺🇸",
+  es: "🇪🇸",
+};
